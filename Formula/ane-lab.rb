e2a3dad4dc039e715614b72ade18a5af2cf796e9
Milad Nekofar <milad@nekofar.com>
2020-09-06 15:08:01 +0430

    feat(anelab): add a basic cask formula for anelab
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

---
+++ Formula/ane-lab.rb
@@ -0,0 +1,11 @@
+cask 'ANE-LAB' do
+  version ''
+  sha256 ''
+
+  url 'https://github.com/myflashlab/ANE-LAB/blob/master/bucket/aneLab.dmg'
+  name ''
+  desc ''
+  homepage 'https://www.myflashlabs.com'
+
+  app 'ANE LAB.app'
+end
