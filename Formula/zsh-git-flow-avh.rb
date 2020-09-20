71aad131038a4bf1d5fc8c9a8e6103809908e3fd
Milad Nekofar <milad@nekofar.com>
2020-09-20 23:21:12 +0430

    fix: solve issues refrenced by test bot
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- Formula/zsh-git-flow-avh.rb
+++ Formula/zsh-git-flow-avh.rb
@@ -2,23 +2,24 @@
 
 # This plugin adds short aliases for the git-flow commands
 class ZshGitFlowAvh < Formula
-  desc 'This plugin adds short aliases for the git-flow commands'
-  homepage 'https://github.com/nekofar/zsh-git-flow-avh'
-  url 'https://github.com/nekofar/zsh-git-flow-avh/archive/v1.0.0.tar.gz'
-  sha256 '1cedc8357420daa1e39d278ed53cf3a5a3591f768898a33a836cf1eab4204dda'
-  head 'https://github.com/nekofar/zsh-git-flow-avh.git', branch: 'master'
-  license 'MIT'
+  desc "This plugin adds short aliases for the git-flow commands"
+  homepage "https://github.com/nekofar/zsh-git-flow-avh"
+  url "https://github.com/nekofar/zsh-git-flow-avh/archive/v1.0.0.tar.gz"
+  sha256 "1cedc8357420daa1e39d278ed53cf3a5a3591f768898a33a836cf1eab4204dda"
+  license "MIT"
+  head "https://github.com/nekofar/zsh-git-flow-avh.git", branch: "master"
 
   bottle :unneeded
 
-  depends_on 'zsh' => :recommended
-  depends_on 'git-flow-avh'
+  depends_on "git-flow-avh"
+  depends_on "zsh" => :recommended
 
   def install
-    pkgshare.install 'zsh-git-flow-avh.zsh'
+    pkgshare.install "zsh-git-flow-avh.zsh"
   end
 
   test do
-    assert_match '1', shell_output("zsh -c '. #{pkgshare}/zsh-git-flow-avh.zsh' && echo $(alias | grep gfli | wc -l)'")
+    assert_match "1",
+      shell_output("zsh -c '. #{pkgshare}/zsh-git-flow-avh.zsh' && echo $(alias | grep gfli | wc -l)'")
   end
 end
