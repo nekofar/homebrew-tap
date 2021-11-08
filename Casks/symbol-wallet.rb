1ca42f6aab503caa1df04e0f4de97910a7a1257e
Milad Nekofar <milad@nekofar.com>
2021-11-08 18:07:35 +0330

    feat(symbol-wallet): add new cask formula for `Symbol Wallet`
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

---
+++ Casks/symbol-wallet.rb
@@ -0,0 +1,18 @@
+cask "symbol-wallet" do
+  version "1.0.8"
+  sha256 "bbb5e725f7d79c0afdccfd4e6692cd311cc47b1b420b64143aa9abc82f7b0b85"
+
+  url "https://github.com/symbol/desktop-wallet/releases/download/v#{version}/symbol-desktop-wallet-mac-#{version}.dmg",
+      verified: "https://github.com/symbol/desktop-wallet/"
+  name "Symbol Desktop Wallet"
+  desc "Client for Symbol to manage accounts, mosaics, namespaces, and issue transaction"
+  homepage "https://symbolplatform.com"
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*.*)$/i)
+  end
+
+  app "Symbol Wallet.app"
+end
