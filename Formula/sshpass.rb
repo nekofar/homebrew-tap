4059d5d7fa1b7e42c7c797941971d329fa405222
Milad Nekofar <milad@nekofar.com>
2021-11-08 13:40:04 +0330

    fix(sshpass): solve some issues detected in formula
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,11 +1,12 @@
-require 'formula'
+# frozen_string_literal: true
 
 class Sshpass < Formula
-  url 'https://sourceforge.net/projects/sshpass/files/sshpass/1.08/sshpass-1.08.tar.gz'
-  homepage 'https://sourceforge.net/projects/sshpass'
-  sha256 '8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02'
+  desc "sshpass is a utility designed for running ssh using the mode referred to as 'keyboard-interactive' password authentication, but in non-interactive mode."
+  homepage "https://sourceforge.net/projects/sshpass"
+  url "https://sourceforge.net/projects/sshpass/files/sshpass/1.08/sshpass-1.08.tar.gz"
+  sha256 "8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02"
 
-  depends_on 'gnu-sed'
+  depends_on "gnu-sed"
 
   def install
     system "./configure", "--disable-debug",
