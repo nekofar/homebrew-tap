f4890926a4baa4cf6c2c1e5be64bda62b03c36a5
Milad Nekofar <milad@nekofar.com>
2021-11-14 18:40:57 +0330

    feat(hiweb-cli): update `HiWeb Cli` sha256 hash
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- Formula/hiweb-cli.rb
+++ Formula/hiweb-cli.rb
@@ -2,7 +2,7 @@ class HiwebCli < Formula
   desc "Simple command-line tool to access HiWeb account information"
   homepage "https://github.com/nekofar/hiweb-cli"
   url "https://github.com/nekofar/hiweb-cli/releases/download/v1.0.0-alpha.6/hiweb.phar"
-  sha256 "d284e10f38a5741be38327d12795ff6971074635ba50531fd1ea54298be9e2d1"
+  sha256 "63d2c57de3636fbbf2432a6c04b466019faf2183949434994ed8ed05e37f259e"
   license "MIT"
 
   livecheck do
