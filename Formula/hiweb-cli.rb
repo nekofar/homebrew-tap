54e2b70877c24b66fc1a86636f6bee8b23ac6ccf
Milad Nekofar <milad@nekofar.com>
2021-11-14 18:13:11 +0330

    feat(hiweb-cli): solve some issues detected by test bot
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- Formula/hiweb-cli.rb
+++ Formula/hiweb-cli.rb
@@ -1,13 +1,13 @@
 class HiwebCli < Formula
-  desc "Hiweb CLI"
+  desc "Simple command-line tool to access HiWeb account information"
   homepage "https://github.com/nekofar/hiweb-cli"
   url "https://github.com/nekofar/hiweb-cli/releases/download/v1.0.0-alpha.6/hiweb.phar"
   sha256 "d284e10f38a5741be38327d12795ff6971074635ba50531fd1ea54298be9e2d1"
   license "MIT"
 
-  depends_on "php"
   depends_on "chromedriver"
   depends_on "geckodriver"
+  depends_on "php"
 
   def install
     bin.install "hiweb.phar" => "hiweb"
