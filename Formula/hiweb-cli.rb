c8c5cf7592332cd8d5e32d1da694d085f10d087a
Milad Nekofar <milad@nekofar.com>
2021-11-14 18:08:40 +0330

    feat(hiweb-cli): add new formula for `HiWeb CLI`
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

---
+++ Formula/hiweb-cli.rb
@@ -0,0 +1,19 @@
+class HiwebCli < Formula
+  desc "Hiweb CLI"
+  homepage "https://github.com/nekofar/hiweb-cli"
+  url "https://github.com/nekofar/hiweb-cli/releases/download/v1.0.0-alpha.6/hiweb.phar"
+  sha256 "d284e10f38a5741be38327d12795ff6971074635ba50531fd1ea54298be9e2d1"
+  license "MIT"
+
+  depends_on "php"
+  depends_on "chromedriver"
+  depends_on "geckodriver"
+
+  def install
+    bin.install "hiweb.phar" => "hiweb"
+  end
+
+  test do
+    system "#{bin}/hiweb", "--version"
+  end
+end
