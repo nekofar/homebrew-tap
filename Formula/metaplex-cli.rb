fc6c127e3b725769edd98a8c75e42acf4b9b84e9
Milad Nekofar <milad@nekofar.com>
2022-01-05 15:34:23 +0330

    feat(metaplex): add a new formula for `metaplex`

---
+++ Formula/metaplex-cli.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class TsNode < Formula
+  desc "Protocol and application framework for decentralized NFT minting, storefronts, and sales."
+  homepage "https://metaplex.com/"
+  url "https://github.com/metaplex-foundation/metaplex/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "33906c31fb4db8a2cb9526013beaba9575c0ea6867f5872024e3499346df6085"
+  license "Apache-2.0"
+  head "https://github.com/metaplex-foundation/metaplex.git", branch: "master"
+
+  depends_on "node"
+  depends_on "typescript"
+  depends_on "ts-node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec), "./js/"
+    # bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/candy-machine-cli", "--version"
+  end
+end
